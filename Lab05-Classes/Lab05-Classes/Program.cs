028cb9a6d9daf51e080ec1fbee9ba26818ae9b31
Le Nhat Tung <lenhattung@gmail.com>
2026-05-05 09:16:28 +0700

    u
    u

---
+++ Lab05-Classes/Lab05-Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Student is a CLASS
+        // s1 is an OBJECT
+        Console.WriteLine("S1: ");
+        Student s1 = new Student();
+        s1.Input();
+        s1.Display();
+        Console.WriteLine("\n\n");
+
+        // s2 is an OBJECT
+        Console.WriteLine("S2: ");
+        Student s2 = new Student(); // Default Constructor
+        s2.Display();
+        Console.WriteLine("\n\n");
+
+        // s3 is an OBJECT
+        Console.WriteLine("S3: ");
+        Student s3 = new Student("S0003", "Nguyen Van A", 2009, "Ho Chi Minh City"); // Parameterized Constructor
+        s3.Display();
+        Console.WriteLine("\n\n");
+
+
+    }
+
+}
---
+++ Lab05-Classes/Lab05-Classes/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+public class Student
+{
+    private string StudentId;
+    private string Name;
+    private int BirthYear;
+    private string Address;
+
+    // Default constructor
+    public Student()
+    {
+        StudentId = "00000";
+        Name = "Unknown";
+        BirthYear = 2010;
+        Address = "N/A";
+    }
+
+    // Parameterized Constructor
+    public Student(string studentId, string name, int birthYear, string address)
+    {
+        StudentId =studentId;
+        Name =name;
+        BirthYear=birthYear;
+        Address =address;
+    }
+
+    public Student(string studentId, string name, int birthYear)
+    {
+        StudentId = studentId;
+        Name = name;
+        BirthYear = birthYear;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"ID: {StudentId}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Birth year: {BirthYear}");
+        Console.WriteLine($"Address: {Address}");
+    }
+
+    public void Input()
+    {
+        Console.Write("Enter Student Id: ");
+        StudentId = Console.ReadLine();
+        Console.Write("Enter Student Name: ");
+        Name = Console.ReadLine();
+        Console.Write("Enter Birth Year: ");
+        BirthYear = int.Parse(Console.ReadLine());
+        Console.Write("Enter Address: ");
+        Address = Console.ReadLine();
+    }
+
+}
